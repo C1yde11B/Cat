9de14bad8d59c5d7d7b9c347a15921f17b82b441
C1yde11B <Clydellb0@gmail.com>
2024-10-10 09:11:16 -0500

    Initial Commit

---
+++ Cat/CatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatApp.swift
+//  Cat
+//
+//  Created by AM Student on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatTests/CatTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatTests.swift
+//  CatTests
+//
+//  Created by AM Student on 10/10/24.
+//
+
+import Testing
+@testable import Cat
+
+struct CatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
